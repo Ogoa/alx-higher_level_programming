e94723bcb17754a09dff0d0aa8fccb789277572a
Ogoa <ogopabhs@gmail.com>
2023-11-29 09:17:47 +0000

    Update: Make insert_node handle edge cases
    The function was updated to handle insertion of a value when the list is
    initially empty and also when there is a duplicate value

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,8 +12,6 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new_node;
 	listint_t *temp;
 
-	if (head == NULL || *head == NULL)
-		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node) /* Check if malloc has failed */
 		return (NULL);
@@ -21,16 +19,24 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->next = NULL;
 
 	/* Inserting the node into the sorted list */
-	temp = *head;
-	while (temp)
+	if (!head || !*head)
 	{
-		if (temp->n < number && temp->next->n > number)
+		*head = new_node;
+		return (new_node);
+	}
+	else
+	{
+		temp = *head;
+		while (temp)
 		{
-			new_node->next = temp->next;
-			temp->next = new_node;
-			return (new_node);
+			if (temp->n <= number && temp->next->n >= number)
+			{
+				new_node->next = temp->next;
+				temp->next = new_node;
+				return (new_node);
+			}
+			temp = temp->next;
 		}
-		temp = temp->next;
 	}
 	return (NULL);
 }
