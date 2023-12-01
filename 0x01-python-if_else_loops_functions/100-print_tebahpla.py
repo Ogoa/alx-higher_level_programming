d4b71909b15ab980b3a092c2676bbcc858326cba
Ogoa <ogopabhs@gmail.com>
2023-12-01 23:31:37 +0000

    Update: Only use one print statement in 100-...

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 count = 0
 for i in range(ord('z'), ord('a') - 1, -1):
-    if count % 2:
-        print(f"{chr(i - 32)}", end="")
-    else:
-        print(f"{chr(i)}", end="")
+    print(f"{chr(i - 32)}" if count % 2 else f"{chr(i)}", end="")
     count += 1
