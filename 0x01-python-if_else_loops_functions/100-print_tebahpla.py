3b1f49197895cbcbf61416bae1ca0247a05caf94
Ogoa <ogopabhs@gmail.com>
2023-12-01 23:37:04 +0000

    Update: Use .format in 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 count = 0
 for i in range(ord('z'), ord('a') - 1, -1):
-    print(f"{chr(i - 32)}" if count % 2 else f"{chr(i)}", end="")
+    print("{}".format(chr(i - 32) if count % 2 else chr(i)), end="")
     count += 1
