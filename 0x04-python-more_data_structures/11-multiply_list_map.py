aef48206a4aff593e6a91da1ca9e65792c36b6f5
Ogoa <ogopabhs@gmail.com>
2023-12-06 21:57:12 +0000

    Update: Use the map function in 11-mulitply

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    return [x * number for x in my_list]
+    return list(map(lambda x: (x * number), my_list))
 
 
 if __name__ == "__main__":
