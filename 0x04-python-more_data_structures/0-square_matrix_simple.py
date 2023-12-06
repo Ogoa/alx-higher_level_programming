f0888a0f6f98d6e4b782b36dee247418fe914eae
Ogoa <ogopabhs@gmail.com>
2023-12-06 13:15:09 +0000

    Update: Improve function in 0-square_matrix
    A change was made to improve the function square_matrix_simple() by
    using a list comprehension. This helped reduce the function to a single
    line compared to the for-loop that was being used previously.

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 # Computes the square values of all integers of a matrix
 def square_matrix_simple(matrix=[]):
-    squares = []
-    for x in matrix:
-        squares.append(list(map(lambda i: (i * i), x)))
-    return squares
+    return list(list(map(lambda x: (x * x), row)) for row in matrix)
 
 
 if __name__ == "__main__":
