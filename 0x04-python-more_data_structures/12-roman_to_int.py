397931239c97177cb727029422e1300cba7b604f
Ogoa <ogopabhs@gmail.com>
2023-12-06 22:29:28 +0000

    Update: Handle edge cases in 12-roman_to_int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,17 @@
 # Function that converst a roman numeral to an integer
 def roman_to_int(roman_string):
     keys = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
-    values = [1, 5, 10, 50, 500, 1000]
+    values = [1, 5, 10, 50, 100, 500, 1000]
     roman_numerals = dict(zip(keys, values))
     if not roman_string or roman_string is None:
         return 0
     integer = 0
-    for i in reversed(roman_string):
-        if i in roman_numerals:
-            integer = integer + roman_numerals[i]
+    for i in range(len(roman_string) - 1):
+        if roman_numerals[roman_string[i]] < roman_numerals[roman_string[i+1]]:
+            integer = integer - roman_numerals[roman_string[i]]
+        else:
+            integer = integer + roman_numerals[roman_string[i]]
+    integer = integer + roman_numerals[roman_string[-1]]
     return integer
 
 
