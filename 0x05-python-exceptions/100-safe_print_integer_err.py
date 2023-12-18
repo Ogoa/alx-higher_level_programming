4ec944acda5e5c9da853c07fe66da5ad5deefa96
Ogoa <ogopabhs@gmail.com>
2023-12-18 21:44:17 +0000

    Update: Handle type error in 100-safe_print

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,10 +6,10 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except ValueError as e:
+        return True
+    except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
-    return True
 
 
 if __name__ == "__main__":
