cfa7220d4332b17c5811eef0140e585c94dfa41c
Ogoa <ogopabhs@gmail.com>
2023-12-18 18:03:25 +0000

    Update: Use 'finally' in 4-list_division.py
    This was done to meet on of the requirements of task 4; using try: /
    except: / finally:

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,7 +16,8 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result.append(0)
-        i = i + 1
+        finally:
+            i = i + 1
     return result
 
 
