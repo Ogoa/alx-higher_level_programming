664b41fc3711ad5834c08c97f1d7fb8a1322cd4d
Ogoa <ogopabhs@gmail.com>
2023-12-19 22:49:56 +0000

    Update: Add a module docstring 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+'''This is defines an empty class "Square"'''
+
+
 class Square:
-    '''Empty class that defines a square'''
+    '''Empty class that defines a square
+
+    Attributes:
+        This class has no attributes
+    '''
     pass
