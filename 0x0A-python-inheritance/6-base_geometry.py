b9918668a9b13f28fc4e85152b823f95b0a81035
Ogoa <ogopabhs@gmail.com>
2024-01-08 23:51:30 +0000

    Add: Complete task 6 of Python Inheritance

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+'''Defines a class BaseGeometry'''
+
+
+class BaseGeometry:
+    '''Definition of the BaseGeometry class'''
+
+    def area(self):
+        '''Function that raises an exception'''
+
+        raise Exception("area() is not implemented")
