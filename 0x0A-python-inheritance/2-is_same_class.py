5f321a45649d61d08990225bce3cce945a19dbd2
Ogoa <ogopabhs@gmail.com>
2024-01-08 23:18:37 +0000

    Add: Complete task 2 of Python Inheritance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Defines a function is_same_class()'''
+
+
+def is_same_class(obj, a_class):
+    '''Evaluates if an object is exactly an instance ofa specified class
+
+    Args:
+        obj (obj): The object being evaluated
+        a_class (obj): The class @obj is being checked against
+
+    Returns:
+        bool: True if @obj is an instance of @a_class, otherwise False
+    '''
+
+    if isinstance(obj, a_class) and a_class is not object:
+        return True
+    return False
