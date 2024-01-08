5772833fa93e048f558cc7088d51dd99b032b82c
Ogoa <ogopabhs@gmail.com>
2024-01-08 23:46:28 +0000

    Update: Insert the __name__ check in 4-inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -17,3 +17,7 @@ def inherits_from(obj, a_class):
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     return False
+
+
+if __name__ == "__main__":
+    inherits_from(obj, a_class)
