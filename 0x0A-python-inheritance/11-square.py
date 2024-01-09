04f1992f207a4cac7089f536f7c8b808abf57cc8
Ogoa <ogopabhs@gmail.com>
2024-01-09 00:32:31 +0000

    Add: Complete task 11 of Python Inheritance

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Defines the class Square'''
+
+
+class Square(__import__("9-rectangle").Rectangle):
+    '''Definition of the class Square
+    Inherits from Rectangle'''
+
+    def __init__(self, size):
+        '''Initialises an instance of Square
+
+        Args:
+            size (int): Dimensions of the square
+        '''
+
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''Evaluates the area of the object
+
+        Returns:
+            int: Area of the object
+        '''
+
+        return (self.__size ** 2)
+
+    def __str__(self):
+        '''Casts a string representation of the object'''
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
