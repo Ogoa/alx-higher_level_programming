4f0fabc42de4bccd3026f70ec69a5994f35c9f56
Ogoa <ogopabhs@gmail.com>
2024-01-09 20:41:26 +0000

    Add: Complete task 5 of Input/Output

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Defines a function save_to_json_file()'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''Writes a serialised JSON string in a text file
+
+    Args:
+        my_obj (obj): Python object
+        filename (str): Name of the text file
+    '''
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json_obj = json.dumps(my_obj)
+        f.write(json_obj)
