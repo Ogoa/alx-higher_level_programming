c7c3028dab86119b0960ca9e241959bb962cb334
Ogoa <ogopabhs@gmail.com>
2024-01-09 20:23:38 +0000

    Add: Complete task 3 of Input/Output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Defines a function to_json_string()'''
+import json
+
+
+def to_json_string(my_obj):
+    '''Serializes an object to a JSON string
+
+    Returns:
+        str: JSON string
+    '''
+
+    return json.dumps(my_obj, sort_keys=True)
