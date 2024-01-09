75d649a2d480387c86dfbc7bab25a9d5fe6e7d4b
Ogoa <ogopabhs@gmail.com>
2024-01-09 20:28:37 +0000

    Add: Complete task 4 of Input/Output

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Defines a function from_json_string()'''
+import json
+
+
+def from_json_string(my_str):
+    '''Deserialize a JSON string to a Python dictionary object
+
+    Returns:
+        dict: The python object
+    '''
+
+    return json.loads(my_str)
