42bb7b10ef004ad3c173644414f2b95d89643d74
Ogoa <ogopabhs@gmail.com>
2024-01-09 15:25:31 +0000

    Add: Complete task 2 of Input/Output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Defines a function append_write()'''
+
+
+def append_write(filename="", text=""):
+    '''Appends a string at the end of a text file
+
+    Returns:
+        int: The number of characters added
+    '''
+
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
