42f6e36a53705a9d7f839684a5ba04a923db840d
Ogoa <ogopabhs@gmail.com>
2024-01-13 02:33:26 +0000

    Add: implement dunder init of Square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Defines a class Square'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Defines the class Square
+    It inherits from the Rectangle class
+    '''
+
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Initialises the object
+
+        Args:
+        size (int): dimensions of the square
+        x (int): x-axis dimensions
+        y (int): y-axis dimensions
+        id (int): identification of the object
+        '''
+
+        super().__init__(size, size, x, y, id)
