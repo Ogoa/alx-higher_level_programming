55875fb5a4d680771dceefc33e14d0d19e901cdb
Ogoa <ogopabhs@gmail.com>
2024-01-14 21:54:11 +0000

    Update: implement size getter and setter tests

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -16,6 +16,24 @@ class TestSquare(unittest.TestCase):
         self.assertEqual((s1.id, s1.width, s1.height, s1.x, s1.y),
                          (1, 3, 3, 1, 2))
 
+    def test_getter_and_setter_methods(self):
+        '''Ensures that the getter and setter methods are
+        working as expected. The setter methods should validate
+        the arguments passed into it and raise type or value
+        errors where necessary
+        '''
+
+        s2 = Square(5, 3, 2, 2)
+        self.assertEqual(s2.size, 5)
+        with self.assertRaises(TypeError):
+            s2.size = True
+            s2.size = "String"
+            s2.size = 5 + 6j
+
+        with self.assertRaises(ValueError):
+            s2.size = -5
+            s2.size = 0
+
 
 if __name__ == "__main__":
     unittest.main()
