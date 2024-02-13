0ac95a7988c4802701a26a12cc8556923ebe5c90
Ogoa <ogopabhs@gmail.com>
2024-02-13 21:40:41 +0000

    Add: Complete task 0, 1 and 2 of SQL

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes the database `hbtn_0c_0` in the MySQL server
+-- Ensures the script does not fail if the database doesn't exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,1 @@
+This subdirectory contains files created under the `0x0D-SQL_introduction` project.
